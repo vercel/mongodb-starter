e6d8cf3e2784f75ede42a18ea17f8542c2998156
Steven Tey <stevensteel97@gmail.com>
2022-06-04 12:40:54 -0500

    added directory home icon for 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,6 @@
 import Meta from '@/components/layout/meta';
+import Link from 'next/link';
+import { DirectoryIcon } from '@/components/icons';
 
 export default function Custom404() {
   return (
@@ -13,8 +15,17 @@ export default function Custom404() {
           ogUrl: 'https://mongodb.vercel.app/404'
         }}
       />
+      <div className="absolute top-6 left-6">
+        <Link href="/">
+          <a>
+            <div className="bg-dark-accent-1 hover:bg-dark-accent-2 transition-all rounded-2xl h-12 w-12 flex justify-center items-center">
+              <DirectoryIcon className="text-white" />
+            </div>
+          </a>
+        </Link>
+      </div>
       <h1 className="text-2xl font-light text-white">
-        404 <span className="mx-3 text-4xl">|</span> Page Not Found
+        404 <span className="mx-3 text-4xl">|</span> User Not Found
       </h1>
     </div>
   );
