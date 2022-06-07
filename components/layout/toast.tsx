9deddd73db87245790de1767ee622df6dc96a228
Steven Tey <stevensteel97@gmail.com>
2022-06-07 17:46:18 -0400

    changed localstorage to cookie for banner

--- components/layout/toast.tsx
+++ components/layout/toast.tsx
@@ -1,79 +1,44 @@
 import { getGradient } from '@/lib/gradients';
-import { useState } from 'react';
-
-function useLocalStorage(key: string, initialValue: any) {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
-  const [storedValue, setStoredValue] = useState(() => {
-    if (typeof window === 'undefined') {
-      return initialValue;
-    }
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key);
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      // If error also return initialValue
-      console.log(error);
-      return initialValue;
-    }
-  });
-
-  const setValue = (value: (arg: any) => any) => {
-    try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      // Save state
-      setStoredValue(valueToStore);
-      // Save to local storage
-      if (typeof window !== 'undefined') {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
-      }
-    } catch (error) {
-      // A more advanced implementation would handle the error case
-      console.log(error);
-    }
-  };
-  return [storedValue, setValue];
-}
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 
 export default function Toast({ username }: { username?: string }) {
-  const [bannerHidden, setBannerHidden] = useLocalStorage(
-    'mongo-banner',
-    false
-  );
+  const [bannerHidden, setBannerHidden] = useState(true);
 
-  if (bannerHidden) {
-    return null;
-  }
+  useEffect(() => {
+    setBannerHidden(Cookies.get('mongo-banner-hidden'));
+  }, []);
 
   return (
-    <div
-      className={`rounded-[16px] ${getGradient(
-        username
-      )} w-11/12 sm:w-[581px] h-[160px] sm:h-[80px] p-0.5 absolute z-10 bottom-10 left-0 right-0 mx-auto`}
-    >
-      <div className="rounded-[14px] w-full h-full bg-[#111111] flex flex-col sm:flex-row items-center justify-center sm:justify-between space-y-3 sm:space-y-0 px-5">
-        <p className="text-white text-[13px] font-mono w-[304px] h-[40px] flex items-center justify-center p-3">
-          Get started with MongoDB Atlas and Vercel instanly.{' '}
-          <button
-            className="contents underline text-blue-400 font-bold"
-            onClick={() => setBannerHidden(true)}
+    !bannerHidden && (
+      <div
+        className={`rounded-[16px] ${getGradient(
+          username
+        )} w-11/12 sm:w-[581px] h-[160px] sm:h-[80px] p-0.5 absolute z-10 bottom-10 left-0 right-0 mx-auto`}
+      >
+        <div className="rounded-[14px] w-full h-full bg-[#111111] flex flex-col sm:flex-row items-center justify-center sm:justify-between space-y-3 sm:space-y-0 px-5">
+          <p className="text-white text-[13px] font-mono w-[304px] h-[40px] flex items-center justify-center p-3">
+            Get started with MongoDB Atlas and Vercel instanly.{' '}
+            <button
+              className="contents underline text-blue-400 font-bold"
+              onClick={() => {
+                setBannerHidden(true);
+                Cookies.set('mongo-banner-hidden', true);
+              }}
+            >
+              Dismiss →
+            </button>
+          </p>
+          <a
+            className="text-white text-[13px] font-mono bg-black border border-[#333333] hover:border-white transition-all rounded-md w-[220px] h-[40px] flex items-center justify-center whitespace-nowrap"
+            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fmongodb-starter&project-name=mongodb-nextjs&repo-name=mongodb-nextjs&demo-title=MongoDB%20Developer%20Directory&demo-description=Log%20in%20with%20GitHub%20to%20create%20a%20directory%20of%20contacts.&demo-url=https%3A%2F%2Fmongodb.vercel.app%2F&demo-image=https%3A%2F%2Fmongodb.vercel.app%2Fog.png&integration-ids=oac_jnzmjqM10gllKmSrG0SGrHOH"
+            target="_blank"
+            rel="noreferrer"
           >
-            Dismiss →
-          </button>
-        </p>
-        <a
-          className="text-white text-[13px] font-mono bg-black border border-[#333333] hover:border-white transition-all rounded-md w-[220px] h-[40px] flex items-center justify-center whitespace-nowrap"
-          href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fmongodb-starter&project-name=mongodb-nextjs&repo-name=mongodb-nextjs&demo-title=MongoDB%20Developer%20Directory&demo-description=Log%20in%20with%20GitHub%20to%20create%20a%20directory%20of%20contacts.&demo-url=https%3A%2F%2Fmongodb.vercel.app%2F&demo-image=https%3A%2F%2Fmongodb.vercel.app%2Fog.png&integration-ids=oac_jnzmjqM10gllKmSrG0SGrHOH"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Clone & Deploy
-        </a>
+            Clone & Deploy
+          </a>
+        </div>
       </div>
-    </div>
+    )
   );
 }
