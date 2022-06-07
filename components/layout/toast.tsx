43c5805af0aedd21682a5ef56bb5b74336318b70
Lee Robinson <lrobinson2011@gmail.com>
2022-06-07 17:13:49 -0400

    Dismissable banner

--- components/layout/toast.tsx
+++ components/layout/toast.tsx
@@ -1,6 +1,54 @@
 import { getGradient } from '@/lib/gradients';
+import { useState } from 'react';
+
+function useLocalStorage(key: string, initialValue: any) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: (arg: any) => any) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+}
 
 export default function Toast({ username }: { username?: string }) {
+  const [bannerHidden, setBannerHidden] = useLocalStorage(
+    'mongo-banner',
+    false
+  );
+
+  if (bannerHidden) {
+    return null;
+  }
+
   return (
     <div
       className={`rounded-[16px] ${getGradient(
@@ -9,7 +57,13 @@ export default function Toast({ username }: { username?: string }) {
     >
       <div className="rounded-[14px] w-full h-full bg-[#111111] flex flex-col sm:flex-row items-center justify-center sm:justify-between space-y-3 sm:space-y-0 px-5">
         <p className="text-white text-[13px] font-mono w-[304px] h-[40px] flex items-center justify-center p-3">
-          Get started with MongoDB Atlas and Vercel in a few clicks
+          Get started with MongoDB Atlas and Vercel instanly, or{' '}
+          <button
+            className="contents underline text-blue-400 font-bold"
+            onClick={() => setBannerHidden(true)}
+          >
+            hide this banner.
+          </button>
         </p>
         <a
           className="text-white text-[13px] font-mono bg-black border border-[#333333] hover:border-white transition-all rounded-md w-[220px] h-[40px] flex items-center justify-center whitespace-nowrap"
