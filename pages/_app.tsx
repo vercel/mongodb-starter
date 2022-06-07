7ae18296e0bf85986b200296aa3eadc2fb90ec5c
Steven Tey <stevensteel97@gmail.com>
2022-06-07 09:32:10 -0400

    remove ModalProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,14 @@
 import '@/styles/globals.css';
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
-import { ModalProvider } from '@/components/modal/use-modal';
 
 export default function MyApp({
   Component,
   pageProps: { session, ...pageProps }
 }: AppProps) {
   return (
-    <ModalProvider>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />{' '}
-      </SessionProvider>
-    </ModalProvider>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />{' '}
+    </SessionProvider>
   );
 }
