3bf2724558426ac80262765ee8dd2e399bdf1d15
Steven Tey <stevensteel97@gmail.com>
2022-06-14 18:17:58 -0500

    Update 500.tsx

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,1 +1,19 @@
 export { getStaticProps } from '.';
+import Meta, { defaultMetaProps } from '@/components/layout/meta';
+
+export default function Custom500() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center bg-black">
+      <Meta
+        props={{
+          ...defaultMetaProps,
+          title: '500 | MongoDB Starter Kit',
+          ogUrl: 'https://mongodb.vercel.app/500'
+        }}
+      />
+      <h1 className="text-2xl font-light text-white">
+        500 <span className="mx-3 text-4xl">|</span> Internal Server Occured
+      </h1>
+    </div>
+  );
+}
